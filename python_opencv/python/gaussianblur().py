63430bfb9f8cedbf7ffce57404737c60e5478363
Dalliaaaaa <110238987+Dalliaaaaa@users.noreply.github.com>
2022-10-03 23:23:02 +0900

    Create gaussianblur().py

---
+++ python_opencv/python/gaussianblur().py
@@ -0,0 +1,9 @@
+import cv2
+import numpy
+src = cv2.imread('python_opencv\images\Kakao_lion.png', cv2.IMREAD_UNCHANGED)
+
+dst = cv2.GaussianBlur(src,(5,5),cv2.BORDER_DEFAULT)
+
+cv2.imshow("Guassian Smoothing", numpy.hstack())
+cv2.waitKey()
+cv2.destroyAllWindows()
