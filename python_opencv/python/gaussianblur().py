17427d6cd8b4e1acb9a4bab08a15f40ff875730b
Dalliaaaaa <110238987+Dalliaaaaa@users.noreply.github.com>
2022-10-07 14:29:49 +0900

    Update gaussianblur().py

--- python_opencv/python/gaussianblur().py
+++ python_opencv/python/gaussianblur().py
@@ -4,6 +4,6 @@ src = cv2.imread('python_opencv\images\Kakao_lion.png', cv2.IMREAD_UNCHANGED)
 
 dst = cv2.GaussianBlur(src,(5,5),cv2.BORDER_DEFAULT)
 
-cv2.imshow("Guassian Smoothing", numpy.hstack(src, dst))
+cv2.imshow("Guassian Smoothing", numpy.hstack(src, dst))  # 주미 바붕ㅎ 내가 해줌.
 cv2.waitKey()
 cv2.destroyAllWindows()
